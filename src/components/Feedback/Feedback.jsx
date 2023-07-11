4dcf595e86205b09a5ee6fa4a4309fbc863dccc2
NataliiaYa <118573896+NataliiaYa@users.noreply.github.com>
2023-07-11 23:35:49 +0300

    fixed

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,27 +1,40 @@
 import React from 'react';
-import { PropTypes } from 'prop-types';
+import { nanoid } from 'nanoid';
+// import { PropTypes } from 'prop-types';
 import css from "./Feedback.module.css";
 
-const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+const FeedbackOptions = ({ onLeaveFeedback }) => {
   return (
-    <div className={css.feedbackOptions}>
-      {options.map(option => (
-        <button
-          key={option}
-          type="button"
-          onClick={() => onLeaveFeedback(option)}
-          className={css.voteButton}
-        >
-          {option}
-        </button>
-      ))}
+    <div>
+      <button
+        key={nanoid()}
+        className={css.voteButton}
+        type="button"
+        name="good"
+        onClick={onLeaveFeedback}
+      >
+        Good
+      </button>
+      <button
+        key={nanoid()}
+        className={css.voteButton}
+        type="button"
+        name="neutral"
+        onClick={onLeaveFeedback}
+      >
+        Neutral
+      </button>
+      <button
+        key={nanoid()}
+        className={css.voteButton}
+        type="button"
+        name="bad"
+        onClick={onLeaveFeedback}
+      >
+        Bad
+      </button>
     </div>
   );
-};
-
-FeedbackOptions.propTypes = {
-  options: PropTypes.arrayOf(PropTypes.string).isRequired,
-  onLeaveFeedback: PropTypes.func,
 };
 
 export default FeedbackOptions;
